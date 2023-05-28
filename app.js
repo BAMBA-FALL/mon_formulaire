da7738425b54a1af9930641bfe8f9d540261b402
unknown <khadimmbackefall1@gmail.comgit config --global user.name BAMBA-FALL>
2023-05-28 14:40:14 +0200

    première partie

---
+++ app.js
@@ -0,0 +1,10 @@
+const user = document.getElementById('user-icon');
+
+user.addEventListener('click',function(){
+
+  const container = document.querySelector('.container')
+  container.classList.toggle('active')
+  
+})
+
+
